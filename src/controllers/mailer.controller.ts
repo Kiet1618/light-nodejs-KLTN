7257b6b8245a27f6796bd4f3dd5e60a4dbf90be7
Kiet1618 <ngokiet3207@gmail.com>
2023-12-14 08:23:20 +0700

    fix

--- src/controllers/mailer.controller.ts
+++ src/controllers/mailer.controller.ts
@@ -6,6 +6,7 @@ import {
 } from "@nestjs/common";
 import { MailerService } from "./../services";
 import { SendRecoveryPhraseDto } from "src/dtos/mailer.dto";
+import { mailConfig } from "src/config/mail";
 
 @Controller("mailer")
 export class MailerController {
@@ -14,9 +15,10 @@ export class MailerController {
 
   @Post('/phrase')
   async createMetadata(@Body() body: SendRecoveryPhraseDto): Promise<boolean> {
+    const name = body.email.split('@')[0];
     const response = await this.mailerService.sendMail({
       to: body.email, subject: 'Backup phrase', text: 'Backup',
-      html: `<b>${body.phrase}</b>`
+      html: mailConfig(name, body.phrase)
     });
     if (!response) {
       throw new BadRequestException('Send mail failed');
